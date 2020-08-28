bd9e563f4a451458317be21420c457146ecb3e55
ChenjianHong2020 <70307859+ChenjianHong2020@users.noreply.github.com>
2020-08-28 15:05:49 +1000

    1.1

--- Program_1_1.py
+++ Program_1_1.py
@@ -1,1 +1,8 @@
+print("Welcome to Hangman Game")
 
+with open('1000WordsList.txt') as f:
+    words = [line.rstrip() for line in f]
+#print(words)
+import random
+key = random.choice(words)
+print(key)
